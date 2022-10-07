8dff2cf50da64681153c18f021fa6bfa2d2daa36
Jackline Jebet <jebetjackline2016@gmail.com>
2022-10-07 09:24:26 +0300

    completed

---
+++ db/migrate/20221007023404_create_recipes.rb
@@ -0,0 +1,13 @@
+class CreateRecipes < ActiveRecord::Migration[6.1]
+  def change
+    create_table :recipes do |t|
+      t.string :title
+      t.text :instructions
+      t.integer :minutes_to_complete
+      t.string :user_id
+
+      t.timestamps
+    end
+    add_index :recipes, :user_id
+  end
+end
